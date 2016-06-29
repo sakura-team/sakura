2f40ce3b2a87de59b4e17ed8730e1245f7e437f2
Etienne Dublé <etienne.duble@imag.fr>
2016-06-29 17:46:32 +0200

    server: Use either rpyc or bottle (bottle part only partial for now)

--- panteda/panteda-server.py
+++ panteda/panteda-server.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-import rpyc
+import rpyc, sys
 from utils import *
 from rpyc.utils.server import ThreadedServer
+from bottle import Bottle
 from PantedaData import ServerPantedaDataOperator
 from PantedaMean import ServerPantedaMeanOperator
 from PantedaSelect import ServerPantedaSelectOperator
 
 DEBUG = False
 
-class PantedaService(rpyc.Service):
+class PantedaService(object):
     def __init__(self, *args, **kwargs):
-        rpyc.Service.__init__(self, *args, **kwargs)
         self.next_op_id = 0
         self.operators = {}
         self.register_operator_classes()
@@ -19,27 +19,28 @@ class PantedaService(rpyc.Service):
             cls.OP_TYPE : cls  \
                 for cls in SERVER_OPERATOR_CLASSES
         }
-    def exposed_register_operator(self, op_type):
+    def register_operator(self, op_type):
         op_id = self.next_op_id
         self.next_op_id += 1
         op_class = self.operator_classes[op_type]
         operator = op_class()
         self.operators[op_id] = operator
+        print 'registered operator ' + op_type
         return op_id
-    def exposed_set_operator_sources(self, op_id, source_op_ids):
+    def set_operator_sources(self, op_id, source_op_ids):
         operator = self.operators[op_id]
         source_ops = [self.operators[i] for i in source_op_ids]
         operator.set_source_ops(source_ops)
-    def exposed_get_operator_iterator(self, op_id):
+    def get_operator_iterator(self, op_id):
         operator = self.operators[op_id]
         return operator.get_iterator()
-    def exposed_get_operator_output_len(self, op_id):
+    def get_operator_output_len(self, op_id):
         operator = self.operators[op_id]
         return operator.get_output_len()
-    def exposed_get_operator_output(self, op_id, i):
+    def get_operator_output(self, op_id, i):
         operator = self.operators[op_id]
         return operator.get_result(i)
-    def exposed_describe_outputs(self, op_id):
+    def describe_outputs(self, op_id):
         operator = self.operators[op_id]
         return tuple([
                 tuple([
@@ -48,11 +49,34 @@ class PantedaService(rpyc.Service):
                     else (col_label, col_type)
                 for col_label, col_type in output])
             for output in operator.describe_outputs()])
-    def exposed_describe_enum(self, op_id, out_id, name):
+    def describe_enum(self, op_id, out_id, name):
         operator = self.operators[op_id]
         for col_name, col_type in operator.describe_outputs()[out_id]:
             if col_name == name:
                 return tuple(col_type.iterkeys())
+
+class RPyCPantedaService(PantedaService, rpyc.Service):
+    def __init__(self, *args, **kwargs):
+        rpyc.Service.__init__(self, *args, **kwargs)
+        PantedaService.__init__(self, *args, **kwargs)
+        self.exposed_register_operator = self.register_operator
+        self.exposed_set_operator_sources = self.set_operator_sources
+        self.exposed_get_operator_iterator = self.get_operator_iterator
+        self.exposed_get_operator_output_len = self.get_operator_output_len
+        self.exposed_get_operator_output = self.get_operator_output
+        self.exposed_describe_outputs = self.describe_outputs
+        self.exposed_describe_enum = self.describe_enum
+
+class BottlePantedaService(PantedaService):
+    def serve(self):
+        app = Bottle()
+
+        @app.route('/operator/register/<op_type>')
+        def register_operator(op_type):
+            return { 'op_id': self.register_operator(op_type) }
+
+        app.run()
+
 
 if __name__ == "__main__":
     SERVER_OPERATOR_CLASSES = [
@@ -60,6 +84,10 @@ if __name__ == "__main__":
         ServerPantedaSelectOperator,
         ServerPantedaDataOperator
     ]
-    server = ThreadedServer(PantedaService, port = 12345)
-    server.start()
+    if len(sys.argv) > 1 and sys.argv[1] == '--rpyc':
+        server = ThreadedServer(RPyCPantedaService, port = 12345)
+        server.start()
+    else:
+        service = BottlePantedaService()
+        service.serve()
 
