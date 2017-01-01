7fe549e2a1d925e505e6690149abd7350c36cd43
Etienne Dublé <etienne.duble@imag.fr>
2017-01-01 16:08:09 +0100

    Update Hub API for the web GUI.

--- sakura/hub/web/api.py
+++ sakura/hub/web/api.py
@@ -3,12 +3,33 @@ class GuiToHubAPI(object):
         self.context = context
 
     def list_daemons(self, *args, **kwargs):
-        daemons = self.context.list_daemons()
-        return daemons
+        return self.context.list_daemons()
 
     def list_operators_classes(self):
         return self.context.list_op_classes()
 
-    def instantiate_operator(self, *args, **kwargs):
-        id = args[0]
-        return ("not yet")
+    # instanciate an operator and return the instance id
+    def create_operator(self, cls_id):
+        raise NotImplementedError
+
+    # delete operator instance and links involved
+    def delete_operator(self, op_id):
+        raise NotImplementedError
+
+    def get_operator_input_info(self, op_id):
+        raise NotImplementedError
+
+    def get_operator_output_info(self, op_id):
+        raise NotImplementedError
+
+    def create_link(self, src_op_id, src_out_id, dst_op_id, dst_in_id):
+        raise NotImplementedError
+
+    def delete_link(self, link_id):
+        raise NotImplementedError
+
+    def get_operator_input_range(self, op_id, in_id, row_start, row_end):
+        raise NotImplementedError
+
+    def get_operator_output_range(self, op_id, out_id, row_start, row_end):
+        raise NotImplementedError
