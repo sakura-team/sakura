ae9732e90deaa432d7f3106a1f9cbb73c66c2cd1
Etienne Dublé <etienne.duble@imag.fr>
2017-01-11 09:04:41 +0100

    DataSample: add one more output table.

--- sakura/operators/datasample/operator.py
+++ sakura/operators/datasample/operator.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
+import random
 from sakura.daemon.processing.operator import Operator
 
-DATA_COLUMNS = (
+DATA1_COLUMNS = (
     ("Name", str), ("Age", int),  ("Gender", str), ("Height", int))
-DATA = (
+DATA1 = (
     ("John", 52, "male", 175),
     ("Alice", 34, "female", 184),
     ("Bob", 31, "male", 156),
     ("Jane", 38, "female", 164)
 )
+DATA2_LENGTH = 1000
 
 class DataSampleOperator(Operator):
     NAME = "Data Sample"
@@ -18,12 +20,17 @@ class DataSampleOperator(Operator):
         # no inputs
         pass
         # outputs
-        self.output_table = self.register_output('Data', self.compute)
-        self.output_table.length = len(DATA)
-        for colname, coltype in DATA_COLUMNS:
-            self.output_table.add_column(colname, coltype)
+        self.output_table1 = self.register_output('Data1', self.compute_table1)
+        self.output_table1.length = len(DATA1)
+        for colname, coltype in DATA1_COLUMNS:
+            self.output_table1.add_column(colname, coltype)
+        self.output_table2 = self.register_output('Data2', self.compute_table2)
+        self.output_table2.add_column('Integers', int)
         # no parameters
         pass
-    def compute(self):
-        for row_idx in range(len(DATA)):
-            yield DATA[row_idx]
+    def compute_table1(self):
+        for row_idx in range(len(DATA1)):
+            yield DATA1[row_idx]
+    def compute_table2(self):
+        for row_idx in range(DATA2_LENGTH):
+            yield (random.randint(0, 1000),)
