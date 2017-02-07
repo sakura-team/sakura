81c758eea0d1b65b6be5d1c88c24479e35d2de23
ortega <michael.ortega@imag.fr>
2017-02-07 15:34:05 +0100

    pagination in operator's modal inputs ; refactoring for outputs

--- workflow/js/modal_operator.js
+++ workflow/js/modal_operator.js
@@ -5,46 +5,9 @@
 var max_rows = 20;
 
 function fill_all(id) {
-    fill_inputs(id);
+    fill_in_out('input', id);
     fill_params(id);
-    fill_outputs(id);
-}
-
-
-function fill_inputs(id) {
-    var cl_id = id.split("_")[1];
-    var inst_id = parseInt(id.split("_")[2]);
-    var nb_inputs = global_ops_cl[cl_id]['inputs'];
-    var nb_outputs = global_ops_cl[cl_id]['outputs'];
-    
-    var d = document.getElementById('modal_'+id+'_tab_inputs');
-    if (nb_inputs == 0) {
-        d.innerHTML = '<br><p align="center"> No Inputs</p>';
-    }
-    else {
-        ws_request('get_operator_instance_info', [inst_id], {}, function (result_info) {
-            ws_request('get_operator_input_range', [inst_id, 0, 0, 1000], {}, function (result_input) {
-                if (result_input) {
-                    s = '<table class="table table-hover table-striped table-sm">\n<thead><tr>';
-                    result_info['inputs'][0]['columns'].forEach( function(item) {
-                        s+= '<th>'+item[0]+'</th>';
-                    });
-                    s += '</tr></thead><tbody>';
-                    result_input.forEach( function(row) {
-                        s += '<tr>\n';
-                        row.forEach( function(col) {
-                            s += '<td>'+col+'</td>'; 
-                        });
-                        s += '</tr>';
-                    });
-                    s += '</tbody></table>';
-                    d.innerHTML = '<br>'+s;
-                }
-                else
-                    d.innerHTML = '<br><p align="center"> No Inputs for Now !!</p>';
-            });
-        });
-    }
+    fill_in_out('output', id);
 }
 
 
@@ -102,77 +65,80 @@ function params_onChange(op_id, param_index, select_id) {
             if (result2)
                 console.log(result2);
             else
-                fill_outputs(op_id);
+                fill_in_out('output', op_id);
         });
     });
 }
 
 
-function fill_outputs(id) {
-    var cl_id = parseInt(id.split("_")[1]);
-    var inst_id = parseInt(id.split("_")[2]);
-    var nb_outputs = global_ops_cl[cl_id]['outputs'];
+function fill_in_out(in_out, id) {
+    var inst_id     = parseInt(id.split("_")[2]);    
+    var d           = document.getElementById('modal_'+id+'_tab_'+in_out+'s');
     
+    //cleaning
+    while (d.firstChild) {
+        d.removeChild(d.firstChild);
+    }
+    
+    //infos
     ws_request('get_operator_instance_info', [inst_id], {}, function (result_info) {
-        var d = document.getElementById('modal_'+id+'_tab_outputs');
-        while (d.firstChild) {
-            d.removeChild(d.firstChild);
-        }
-        //console.log(result_info['outputs'][0]['label']);
-        if (nb_outputs == 0) {
-            d.innerHTML = '<br><p align="center"> No Outputs</p>';
+        var nb_in_out = result_info[in_out+'s'].length;
+        
+        if (nb_in_out == 0) {
+            d.innerHTML = '<br><p align="center"> No '+in_out+'s</p>';
         }
         else {
             var div_tab = document.createElement('div');
             div_tab.className = 'modal-body';
-            div_tab.id = id+'_outputs';
+            div_tab.id = id+'_'+in_out+'s';
             
             var ul          = document.createElement('ul');
             var tab_content = document.createElement('div');
             ul.className            = "nav nav-tabs";
             tab_content.className   = "tab-content";
             s = '<li class="active"> \
-                    <a data-toggle="tab" href="#'+id+'_output_'+0+'" onclick=\'fill_one_output(\"'+id+'\",'+0+','+0+','+max_rows+');\'>'+result_info['outputs'][0]['label']+'</a></li>';
-            for (var i =1; i < nb_outputs; i++) {
-                s += '<li><a data-toggle="tab" href="#'+id+'_output_'+i+'" onclick=\'fill_one_output(\"'+id+'\",'+i+','+0+','+max_rows+');\'>'+result_info['outputs'][i]['label']+'</a></li>';
+                    <a data-toggle="tab" href="#'+id+'_'+in_out+'_'+0+'" onclick=\'fill_one_in_out(\"'+in_out+'\",\"'+id+'\",'+0+','+0+','+max_rows+');\'>'+result_info[in_out+'s'][0]['label']+'</a></li>';
+            for (var i =1; i < nb_in_out; i++) {
+                s += '<li><a data-toggle="tab" href="#'+id+'_'+in_out+'_'+i+'" onclick=\'fill_one_in_out(\"'+in_out+'\",\"'+id+'\",'+i+','+0+','+max_rows+');\'>'+result_info[in_out+'s'][i]['label']+'</a></li>';
             }
             ul.innerHTML = s;
-            
-            s = '<div id="'+id+'_output_'+0+'" class="tab-pane fade in active"></div>';
-            for (var i =1; i < nb_outputs; i++)
-                s += '<div id="'+id+'_output_'+i+'" class="tab-pane fade in active"></div>';
+        
+            s = '<div id="'+id+'_'+in_out+'_'+0+'" class="tab-pane fade in active"></div>';
+            for (var i =1; i < nb_in_out; i++)
+                s += '<div id="'+id+'_'+in_out+'_'+i+'" class="tab-pane fade in active"></div>';
             tab_content.innerHTML = s;
-            
+        
             div_tab.appendChild(ul);
             div_tab.appendChild(tab_content);
             d.appendChild(div_tab);
-            
-            fill_one_output(id, 0, 0, max_rows);
+        
+            fill_one_in_out(in_out, id, 0, 0, max_rows);
         }
     });
 }
 
 
-function fill_one_output(id, id_output, min, max) {
-    var div = document.getElementById(id+'_output_'+id_output);
+function fill_one_in_out(in_out, id, id_in_out, min, max) {
+    var d = document.getElementById(id+'_'+in_out+'_'+id_in_out);
     var inst_id = parseInt(id.split("_")[2]);
     
-    //Emptying the div
-    while (div.firstChild) {
-        div.removeChild(div.firstChild);
+    //cleaning
+    while (d.firstChild) {
+        d.removeChild(d.firstChild);
     }
     
+    //infos
     ws_request('get_operator_instance_info', [inst_id], {}, function (result_info) {        
-        ws_request('get_operator_output_range', [inst_id, id_output, min, max], {}, function (result_output) {
+        ws_request('get_operator_'+in_out+'_range', [inst_id, id_in_out, min, max], {}, function (result_in_out) {
             s = '<table class="table table-sm table-hover table-striped">\n<thead><tr>';
             s += '<th>#</th>';
-            result_info['outputs'][id_output]['columns'].forEach( function(item) {
+            result_info[in_out+'s'][id_in_out]['columns'].forEach( function(item) {
                 s+= '<th>'+item[0]+'</th>';
             });
             s += '</tr></thead><tbody>';
             
             var index = 0;
-            result_output.forEach( function(row) {
+            result_in_out.forEach( function(row) {
                 s += '<tr>\n';
                 s += '<td>'+parseInt(index+min)+'</td>';
                 row.forEach( function(col) {
@@ -183,26 +149,26 @@ function fill_one_output(id, id_output, min, max) {
             });
             s += '</tbody></table>';
             
-            if (result_info['outputs'][id_output]['length'] != null) {
-                var nb_pages = parseInt(result_info['outputs'][id_output]['length']/(max-min));
-                if (nb_pages*(max-min) < result_info['outputs'][id_output]['length'])
+            if (result_info[in_out+'s'][id_in_out]['length'] != null) {
+                var nb_pages = parseInt(result_info[in_out+'s'][id_in_out]['length']/(max-min));
+                if (nb_pages*(max-min) < result_info[in_out+'s'][id_in_out]['length'])
                     nb_pages++;
                 if (nb_pages > 1 && nb_pages < 10) {
                     s+= '   <ul class="pagination pagination-sm">\n';
                     for (var i=0; i< nb_pages; i++)
-                        s+= '<li><a style="cursor: pointer;" onclick=\'fill_one_output(\"'+id+'\",'+id_output+','+(i*(max-min))+','+((i+1)*(max-min))+');\'>'+(i+1)+'</a></li>\n';
+                        s+= '<li><a style="cursor: pointer;" onclick=\'fill_one_in_out(\"'+in_out+'\",\"'+id+'\",'+id_in_out+','+(i*(max-min))+','+((i+1)*(max-min))+');\'>'+(i+1)+'</a></li>\n';
                     s+= '   </ul>';
                 }
                 else if (nb_pages > 10) {
                     var current_page = Math.floor(min/(max-min));
                     s+= '   <ul class="pagination pagination-sm">\n';
                     if (current_page > 0) {
-                        s += '<li><a style="cursor: pointer;" onclick=\'fill_one_output(\"'+id+'\",'+id_output+','+0+','+(max-min)+');\'>'+'<<'+'</a></li>\n';
-                        s += '<li><a style="cursor: pointer;" onclick=\'fill_one_output(\"'+id+'\",'+id_output+','+(min - (max-min))+','+(max - (max-min))+');\'>'+'<'+'</a></li>\n';
+                        s += '<li><a style="cursor: pointer;" onclick=\'fill_one_in_out(\"'+in_out+'\",\"'+id+'\",'+id_in_out+','+0+','+(max-min)+');\'><span class="glyphicon glyphicon-fast-backward" style="color: grey; cursor: pointer;"></a></li>\n';
+                        s += '<li><a style="cursor: pointer;" onclick=\'fill_one_in_out(\"'+in_out+'\",\"'+id+'\",'+id_in_out+','+(min - (max-min))+','+(max - (max-min))+');\'><span class="glyphicon glyphicon-backward" style="color: grey; cursor: pointer;"></a></li>\n';
                     }
                     else {
-                        s += '<li class="disabled"><a style="cursor: pointer;">'+'<<'+'</a></li>\n';
-                        s += '<li class="disabled"><a style="cursor: pointer;">'+'<'+'</a></li>\n';
+                        s += '<li class="disabled"><a style="cursor: pointer;"><span class="glyphicon glyphicon-fast-backward" style="color: grey; cursor: pointer;"></a></li>\n';
+                        s += '<li class="disabled"><a style="cursor: pointer;"><span class="glyphicon glyphicon-backward" style="color: grey; cursor: pointer;"></a></li>\n';
                     }
                     var up_limit = current_page+10;
                     if (up_limit > nb_pages) {
@@ -213,12 +179,12 @@ function fill_one_output(id, id_output, min, max) {
                             s+= '<li class="disabled"><a style="cursor: pointer;");\'>'+(i+1)+'</a></li>\n';
                         }
                         else {
-                            s+= '<li><a style="cursor: pointer;" onclick=\'fill_one_output(\"'+id+'\",'+id_output+','+(i*(max-min))+','+((i+1)*(max-min))+');\'>'+(i+1)+'</a></li>\n';
+                            s+= '<li><a style="cursor: pointer;" onclick=\'fill_one_in_out(\"'+in_out+'\",\"'+id+'\",'+id_in_out+','+(i*(max-min))+','+((i+1)*(max-min))+');\'>'+(i+1)+'</a></li>\n';
                         }
                     
                     if (up_limit < nb_pages) {
-                        s += '<li><a style="cursor: pointer;" onclick=\'fill_one_output(\"'+id+'\",'+id_output+','+((current_page+1)*(max-min))+','+((current_page+2)*(max-min))+');\'>'+'>'+'</a></li>\n';
-                        s += '<li><a style="cursor: pointer;" onclick=\'fill_one_output(\"'+id+'\",'+id_output+','+((nb_pages-1)*(max-min))+','+((nb_pages)*(max-min))+');\'>'+'>>'+'</a></li>\n';
+                        s += '<li><a style="cursor: pointer;" onclick=\'fill_one_in_out(\"'+in_out+'\",\"'+id+'\",'+id_in_out+','+((current_page+1)*(max-min))+','+((current_page+2)*(max-min))+');\'><span class="glyphicon glyphicon-forward" style="color: grey; cursor: pointer;"></a></li>\n';
+                        s += '<li><a style="cursor: pointer;" onclick=\'fill_one_in_out(\"'+in_out+'\",\"'+id+'\",'+id_in_out+','+((nb_pages-1)*(max-min))+','+((nb_pages)*(max-min))+');\'><span class="glyphicon glyphicon-fast-forward" style="color: grey; cursor: pointer;"></a></li>\n';
                     }
                     s+= '   </ul>';
                 }
@@ -226,14 +192,18 @@ function fill_one_output(id, id_output, min, max) {
             else {
                 s+= '   <ul class="pagination pagination-sm">\n';
                 if (min > 0) {
-                    s += '<li><a style="cursor: pointer;" onclick=\'fill_one_output(\"'+id+'\",'+id_output+','+0+','+(max-min)+');\'>'+'<<'+'</a></li>\n';
-                    s += '<li><a style="cursor: pointer;" onclick=\'fill_one_output(\"'+id+'\",'+id_output+','+(min - (max-min))+','+(max - (max-min))+');\'>'+'<'+'</a></li>\n';
+                    s += '<li><a style="cursor: pointer;" onclick=\'fill_one_in_out(\"'+in_out+'\",\"'+id+'\",'+id_in_out+','+0+','+(max-min)+');\'><span class="glyphicon glyphicon-fast-backward" style="color: grey; cursor: pointer;"></a></li>\n';
+                    s += '<li><a style="cursor: pointer;" onclick=\'fill_one_in_out(\"'+in_out+'\",\"'+id+'\",'+id_in_out+','+(min - (max-min))+','+(max - (max-min))+');\'><span class="glyphicon glyphicon-backward" style="color: grey; cursor: pointer;"></a></li>\n';
                 }
-                if (!(result_output.length < max-min))
-                    s += '<li><a style="cursor: pointer;" onclick=\'fill_one_output(\"'+id+'\",'+id_output+','+(min + (max-min))+','+(max + (max-min))+');\'>'+'>'+'</a></li>\n';
+                else {
+                    s += '<li class="disabled"><a style="cursor: pointer;" ><span class="glyphicon glyphicon-fast-backward" style="color: grey; cursor: pointer;"></a></li>\n';
+                    s += '<li class="disabled"><a style="cursor: pointer;" ><span class="glyphicon glyphicon-backward" style="color: grey; cursor: pointer;"></a></li>\n';
+                }
+                if (!(result_in_out.length < max-min))
+                    s += '<li><a style="cursor: pointer;" onclick=\'fill_one_in_out(\"'+in_out+'\",\"'+id+'\",'+id_in_out+','+(min + (max-min))+','+(max + (max-min))+');\'><span class="glyphicon glyphicon-forward" style="color: grey; cursor: pointer;"></a></li>\n';
                 s+= '   </ul>';
             }
-            div.innerHTML = s;
+            d.innerHTML = s;
         });
     });
 }
