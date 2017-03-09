4db0b97b5eb02dcfe32b26a3e1c4b82a820229df
denisb <denis.bouhineau@imag.fr>
2017-03-09 17:26:29 +0100

    production d'un tableau des protocoles à partir d'une liste, comme si cette liste venait d'une bd

--- web_interface/js/web_interface.js
+++ web_interface/js/web_interface.js
@@ -31,28 +31,62 @@ function showDiv(event,dir) {
   bct = bct + "<li class='active'>"+dirs[i]+"</li>";
   var d = document.getElementById("breadcrumbtrail");
   d.innerHTML = bct;
-  if (idDir=="idDivProtocols") {  //regarder : document.getElementById(idDir).getElementsByClassName("executeOnShow")[i].href
-    listRequestStub(10,false);}
+  var actionsOnShow=document.getElementById(idDir).getElementsByClassName("executeOnShow");
+  for(i=0;i<actionsOnShow.length;i++) {
+	eval(actionsOnShow[i].href);}
   event.preventDefault();}
 
-function buildListStub(result) {
-s="<ul>"
+function buildListStub(idDiv,result) {
+/* s="<ul>"; for(i=0;i<result.length;i++) {
+   s = s + "<li>Experiment <a onclick=\"showDiv(event,'Protocols/tmpProtocol');\" href=\"http://sakura.imag.fr/Protocols/tmpProtocol\">tmpProtocol_"+result[i].nom+"</a></li>";}
+   s+="</ul>"; */
+s="";
 for(i=0;i<result.length;i++) {
-  s = s + "<li>Experiment <a onclick=\"showDiv(event,'Protocols/tmpProtocol');\" href=\"http://sakura.imag.fr/Protocols/tmpProtocol\">tmpProtocol_"+result[i].nom+"</a></li>";}
-s+="</ul>"	 
-document.getElementById("dFF").innerHTML = s;}
+  if (result[i].isPublic=="true") {
+    eyeIcon = "glyphicon-eye-open";}
+  else {
+	eyeIcon = "glyphicon-eye-close";}  
+  s = s + "<tr><td>"+result[i].name+"</td>\n"
+        + "<td>"+result[i].shortDesc+"</td>"
+		+ "<td align='center'><a onclick=\"showDiv(event,'Protocols/tmpProtocol');\" href=\"http://sakura.imag.fr/Protocols/tmpProtocol\" class='btn btn-default'><span class='glyphicon "+eyeIcon+"' aria-hidden='true'></span></a></td>"
+        + "</tr>";}
+document.getElementById(idDiv).innerHTML = s;}
 
+var firstNames=["Geom","Math","Plus","Tutor","Class","Copex","Diag","Form","Lab","Mooc","Mood","Smart","Qcm","Tamag","Tit"];
+var lastNames=["Exp","Elec","Aplus","Edit","Eval","Alg","Add"];
+var lastDigits=["","7","1","2","3","1.2","2.0","1.0","2.1","1997","2000","2001","2002","2009","2014","2015","2016","2017"];
+
+function fullNameAlea() {
+  return firstNames[Math.floor(Math.random() * firstNames.length)]
+    + lastNames[Math.floor(Math.random() * lastNames.length)]+
+    + lastDigits[Math.floor(Math.random() * lastDigits.length)];}
 	
-function listRequestStub(n,bd) {
-if (!bd) {
-// version local
+var firstWords = ["Ipse","Ergo","Hinc","Tempus","Non","Fiat","Logos","Gnove","Lorem"];
+var otherWords = ["fugit","est","veni","vidi","vici","etiam","porro","quisquam","qui","dolorem","ipsum","quia","dolor","sit","amet","adipisci","velit"];
+
+function shortDescAlea() {
+  return firstWords[Math.floor(Math.random() * firstWords.length)]+" "
+    + otherWords[Math.floor(Math.random() * otherWords.length)]+" "
+    + otherWords[Math.floor(Math.random() * otherWords.length)]+" "
+    + otherWords[Math.floor(Math.random() * otherWords.length)]+", "
+    + otherWords[Math.floor(Math.random() * otherWords.length)]+" "
+    + otherWords[Math.floor(Math.random() * otherWords.length)]+" "
+    + otherWords[Math.floor(Math.random() * otherWords.length)]+".";}
+	
+function publicAlea() {
+if (Math.random()>0.2) {
+  return "true";}
+else {
+  return "false";}}  
+
+function listRequestStub(idDiv,n,bd) {
+if (!bd) {  // version local
   result=new Array();
   for(i=0;i<n;i++) {
-    result.push({"nom":"nom"+i,"valeur":"val"+i});}
-  buildListStub(result);}
-else {
-// version réseau
-  ws_request('list_nObjets', [10,'etude_'], {}, function (result) {buildListStub(result);});}
+    result.push({"name":fullNameAlea(),"shortDesc":shortDescAlea(),"isPublic":publicAlea()});}
+  buildListStub(idDiv,result);}
+else {     // version réseau
+  ws_request('list_nObjets', [10,'etude_'], {}, function (idDiv,result) {buildListStub(result);});}
 return ;}
  
  
