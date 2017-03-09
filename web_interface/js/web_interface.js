7ce77fcda951f1714ab33f4e9b045bf6c35948e8
denisb <denis.bouhineau@imag.fr>
2017-03-09 10:23:01 +0100

    reorganisation de protocol (en attente d'un onglet work (design study ?) deux onglets seulement : main (description), historic, et index pour la liste des protocoles

---
+++ web_interface/js/web_interface.js
@@ -0,0 +1,35 @@
+// LIG March 2017
+
+function showDiv(event,dir) {
+  if (dir=="") {
+    dir="Home";}
+  var dirs = dir.split("/");
+//show div
+  mainDivs=document.getElementsByClassName('classMainDiv');
+  for(i=0;i<mainDivs.length;i++) {
+	mainDivs[i].style.display='none';}  
+  var idDir = "idDiv"+dirs.join("");
+  document.getElementById(idDir).style.display='inline';
+//activate navbar   
+  var d = document.getElementById("navbar_ul");
+  for (var i=0; i< d.children.length; i++) {
+    d.children[i].className = "";}
+  var navBarElt = document.getElementById("idNavBar"+dirs[0])
+  if (navBarElt) {
+    navBarElt.className = "active";}
+//set url
+  var stateObj = { foo: "bar" };
+  try {  //try catch, car en local, cela soulève une securityError pour des raisons de same origin policy pas gérées de la meme manière  ...
+  history.pushState(stateObj, "page 2", "bar.html"); } catch (e) { tmp=0;}
+//set breadcrumb
+  var bct = "<li><a onclick=\"showDiv(event,'');\" href=\"http://sakura.imag.fr\" title=\"Sakura\">Sakura</a></li>";
+  var tmpDir = "";
+  for(i=0;i<dirs.length-1;i++) {
+	tmpDir = tmpDir + dirs[i] ;
+	bct = bct + "<li><a onclick='showDiv(event,\""+tmpDir+"\");' href=\"http://sakura.imag.fr/"+tmpDir+"\" title= \""+tmpDir+"\">"+dirs[i]+"</a></li>";
+	tmpDir = tmpDir + "/";}	  
+  bct = bct + "<li class='active'>"+dirs[i]+"</li>";
+  var d = document.getElementById("breadcrumbtrail");
+  d.innerHTML = bct;
+  event.preventDefault();}
+
