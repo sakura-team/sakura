971ca97fa0b00c287c006e39982d9b4138ce7f44
Etienne Dublé <etienne.duble@imag.fr>
2017-03-17 10:52:31 +0100

    Conf: handle default values

--- sakura/common/conf.py
+++ sakura/common/conf.py
@@ -1,6 +1,6 @@
 import json
 
-def merge_args_and_conf(parser):
+def merge_args_and_conf(parser, defaults=None):
     args_conf = parser.parse_args()
     file_conf = json.load(args_conf.conf_file)
 
@@ -11,5 +11,11 @@ def merge_args_and_conf(parser):
         # prefer the value specified in the command line if any
         if name not in conf or getattr(conf, name) == None:
             setattr(conf, name, value)
+
+    if defaults != None:
+        for name, value in defaults.items():
+            if name not in conf:
+                setattr(conf, name, value)
+
     return conf
 
