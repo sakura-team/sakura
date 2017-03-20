dc4e34f57ef3413ab90c1e51e62e34bb7b021470
Etienne Dublé <etienne.duble@imag.fr>
2017-03-20 14:48:13 +0100

    Update central storage schema.

--- sakura/hub/storage.py
+++ sakura/hub/storage.py
@@ -3,19 +3,26 @@ from sakura.common.sqlite import SQLiteDB
 
 DB_SCHEMA = """
 CREATE TABLE IF NOT EXISTS Project (
-    project_id SERIAL PRIMARY KEY,
+    project_id INTEGER PRIMARY KEY AUTOINCREMENT,
     gui_data TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Daemon (
-    daemon_id SERIAL PRIMARY KEY,
-    connected BOOLEAN
+    daemon_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS OpClass (
+    cls_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    daemon_id INTEGER REFERENCES Daemon(daemon_id),
+    name TEXT,
+    UNIQUE(daemon_id, name)
 );
 
 CREATE TABLE IF NOT EXISTS OpInstance (
-    op_id SERIAL PRIMARY KEY,
-    project_id REFERENCES Project(project_id),
-    daemon_id REFERENCES Daemon(daemon_id)
+    op_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    project_id INTEGER REFERENCES Project(project_id),
+    cls_id INTEGER REFERENCES OpClass(cls_id)
 );
 """
 
