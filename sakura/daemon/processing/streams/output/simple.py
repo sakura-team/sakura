052155684438c767ebdaffc589e0db2594348864
Etienne Dublé <etienne.duble@imag.fr>
2017-05-24 15:51:55 +0200

    SimpleStream: Use numpy chunks, reuse last iterator
    We can now iterate over such a stream chunk by chunk.
    Each chunk is implemented as a numpy array. This will allow:
    * faster serialization / deserialization
    * efficient processing (column selection, etc)
    
    get_range() now relies on this feature (thus operators
    distributed over 2 different daemons will now transfer
    numpy arrays), and tries to reuse the same chunk iterator
    accross successive calls.

--- sakura/daemon/processing/streams/output/simple.py
+++ sakura/daemon/processing/streams/output/simple.py
@@ -1,3 +1,5 @@
+import numpy as np
+from itertools import islice
 from sakura.daemon.processing.tools import Registry
 from sakura.daemon.processing.column import Column
 
@@ -7,6 +9,7 @@ class SimpleStream(Registry):
         self.label = label
         self.compute_cb = compute_cb
         self.length = None
+        self.last_iter_info = None
     def add_column(self, col_label, col_type, col_tags=()):
         return self.register(self.columns, Column,
                     col_label, col_type, tuple(col_tags),
@@ -19,13 +22,30 @@ class SimpleStream(Registry):
         for row in self.compute_cb():
             yield row
     def get_range(self, row_start, row_end):
-        rows = []
-        row_idx = 0
-        for row in self:
-            if row_idx == row_end:
+        # try to reuse the last iterator if we scroll at the same offset
+        # with same chunk size
+        it = None
+        if self.last_iter_info is not None:
+            offset, chunk_size, last_it = self.last_iter_info
+            if offset == row_start and chunk_size == row_end - row_start:
+                it = last_it
+        # otherwise, create a new iterator
+        if it == None:
+            it = self.__iter_chunks__(row_end-row_start, row_start)
+        # read next chunk and return it
+        for chunk in it:
+            # update info about last iterator
+            self.last_iter_info = row_start + chunk.size, row_end - row_start, it
+            return chunk
+        # if we are here, stream has ended, return empty range
+        return np.empty((0,), self.get_dtype())
+    def get_dtype(self):
+        return np.dtype(list((col.label, col.type) for col in self.columns))
+    def __iter_chunks__(self, chunk_size, offset=0):
+        dtype = self.get_dtype()
+        it = islice(self.compute_cb(), offset, None)
+        while True:
+            chunk = np.fromiter(islice(it, chunk_size), dtype).view(np.recarray)
+            if chunk.size == 0:
                 break
-            if row_start <= row_idx:
-                rows.append(row)
-            row_idx += 1
-        return rows
-
+            yield chunk
