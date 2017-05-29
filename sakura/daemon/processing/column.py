c6146035104a3e917fdc2fd4ed8606f43d7005b3
Etienne Dublé <etienne.duble@imag.fr>
2017-05-29 15:37:05 +0200

    Introduced NumpyStream class.
    NumpyStream allows to build an output stream
    from a numpy array.
    NumpyStream is now used in datasample/gpsparis.

--- sakura/daemon/processing/column.py
+++ sakura/daemon/processing/column.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Column(object):
     def __init__(self, col_label, col_type, col_tags,
                 output_stream, col_index):
@@ -7,7 +9,9 @@ class Column(object):
         self.output_stream = output_stream
         self.index = col_index
     def get_info_serializable(self):
-        return (self.label, self.type.__name__, self.tags)
+        return (self.label, str(np.dtype(self.type)), self.tags)
     def __iter__(self):
         for row in self.output_stream:
             yield row[self.index]
+    def add_tags(self, *tags):
+        self.tags += tags
--- sakura/daemon/processing/stream.py
+++ sakura/daemon/processing/stream.py
@@ -1,3 +1,4 @@
 from sakura.daemon.processing.streams.input.input import InputStream
 from sakura.daemon.processing.streams.output.simple import SimpleStream
+from sakura.daemon.processing.streams.output.npstream import NumpyStream
 from sakura.daemon.processing.streams.internal.internal import InternalStream
