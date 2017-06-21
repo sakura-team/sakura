7e470119528ca5720e2a84d65dff860c82f13a18
user <user@wificampus-036202.grenet.fr>
2017-06-21 10:06:51 +0200

    [ADDITION] add new hub.conf for tweetsmap, add command in on_exit

--- test/run-hub.sh
+++ test/run-hub.sh
@@ -12,20 +12,35 @@ fi
 on_exit()
 {
     rm -rf $TMPDIR
+    # Delete all operators created for Tweetsmap 
+    rm -rf $HOME/.sakuraTweetsmap
+    echo "Deleted Tweetsmap operators"
 }
 
 # whatever happens, call on_exit() at the end.
 trap on_exit EXIT
 
 # prepare hub conf
+if [ "$1" == "" ]
+then
 cat > $TMPDIR/hub.conf << EOF
-{
-    "web-port": 8081,
-    "hub-port": 10432,
-    "external-datasets": [ ],
-    "work-dir": "$HOME/.sakura"
-}
+    {
+        "web-port": 8081,
+        "hub-port": 10432,
+        "external-datasets": [ ],
+        "work-dir": "$HOME/.sakura"
+    }
 EOF
+else
+cat > $TMPDIR/hub.conf << EOF
+    {
+    "web-port": 8080,
+    "hub-port": 10432,
+    "external-datasets":[],
+    "work-dir": "$HOME/.sakuraTweetsmap"
+    }
+EOF
+fi
 
 ./hub.py -f $TMPDIR/hub.conf $WEBAPP
 
