c6b19df4322bb8bafc7f7578457efc31af0fb149
Etienne Dublé <etienne.duble@imag.fr>
2017-07-04 17:12:55 +0200

    datasample/integers: output 10 millions ints, use chunks

--- sakura/operators/public/datasample/datasets/integers.py
+++ sakura/operators/public/datasample/datasets/integers.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
-import random
-from sakura.daemon.processing.stream import ComputedStream
+import numpy as np
+from sakura.daemon.processing.stream import ComputedStream, ComputeMode
 
-DATA_LEN = 1000
-DATA = tuple((random.randint(0, 1000),) for i in range(DATA_LEN))
+CHUNK_SIZE = int(1E+5)
+LEN = int(1E+7)
+NUM_CHUNKS = int(LEN / CHUNK_SIZE)
+COL_TYPE = ('Integers', np.uint16)
 
 def compute():
-    for row in DATA:
-        yield row
+    for _ in range(NUM_CHUNKS):
+        chunk = np.random.randint(0, 1000, CHUNK_SIZE, np.uint16)
+        yield chunk.astype([COL_TYPE])
 
 # dataset description
-STREAM = ComputedStream('Random integers', compute)
-STREAM.add_column('Integers', int)
-STREAM.length = DATA_LEN
+STREAM = ComputedStream(
+            'Random integers',
+            compute,
+            compute_mode = ComputeMode.CHUNKS)
+STREAM.add_column(*COL_TYPE)
+STREAM.length = LEN
