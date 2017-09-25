d2c3320b989c326553c3a348b92f7ceacb90985e
Etienne Dublé <etienne.duble@imag.fr>
2017-09-25 14:57:19 +0200

    sqlite: use placeholders, add boolean support

--- sakura/common/sqlite.py
+++ sakura/common/sqlite.py
@@ -1,14 +1,9 @@
 import sqlite3
 from pathlib import Path
 
-QUOTE="'"
-
-def quoted(string):
-    s = str(string)
-    if s == 'NULL':
-        return s
-    else:
-        return QUOTE + s + QUOTE
+# boolean handling
+sqlite3.register_adapter(bool, int)
+sqlite3.register_converter("BOOLEAN", lambda v: bool(int(v)))
 
 class SQLiteDB():
 
@@ -17,7 +12,7 @@ class SQLiteDB():
         parent_dir = Path(path).parent
         if not parent_dir.exists():
             parent_dir.mkdir(parents=True)
-        self.c = sqlite3.connect(path)
+        self.c = sqlite3.connect(path, detect_types=sqlite3.PARSE_DECLTYPES)
         # allow name-based access to columns
         self.c.row_factory = sqlite3.Row
 
@@ -36,89 +31,79 @@ class SQLiteDB():
 
     # from a dictionary of the form <col_name> -> <value>
     # we want to filter-out keys that are not column names,
-    # format the value for usage in an SQL statement,
-    # and return (<col_name>, <value>) tuples.
-    def get_tuples(self, table, dictionary):
+    # and return ([<col_name>, ...], [<value>, ...]).
+    def get_cols_and_values(self, table, dictionary):
         # retrieve fields names for this table
         table_desc = self.c.execute("PRAGMA table_info(%s)" % table)
         col_names = set([ col_desc[1] for col_desc in table_desc ])
-
         res = {}
         for k in dictionary:
             # filter-out keys of dictionary that are not
             # a column name
             if k not in col_names:
                 continue
-
-            # format the value appropriately for an SQL
-            # statement
-            value = dictionary[k]
-            if value == None:
-                value = 'NULL'
-            else:
-                value = quoted(value)
-
             # store in the result dict
-            res[k] = value
-        # we prefer a list of (k,v) items instead of
+            res[k] = dictionary[k]
+        # we prefer a tuple ([k,...],[v,...]) instead of
         # a dictionary, because in an insert query,
         # we need a list of keys and a list of values
         # with the same ordering.
-        return res.items()
+        items = res.items()
+        return (list(t[0] for t in items),
+                list(t[1] for t in items))
 
     # allow statements like:
     # db.insert("network", ip=ip, switch_ip=swip)
     def insert(self, table, **kwargs):
         # insert and return True or return False
-        tuples = self.get_tuples(table, kwargs)
+        cols, values = self.get_cols_and_values(table, kwargs)
         cursor = self.c.cursor()
         try:
             cursor.execute("""INSERT INTO %s(%s)
                 VALUES (%s);""" % (
                     table,
-                    ','.join(t[0] for t in tuples),
-                    ','.join(t[1] for t in tuples)))
+                    ','.join(cols),
+                    ','.join(['?'] * len(values))),
+                tuple(values))
             self.lastrowid = cursor.lastrowid
             return True
         except sqlite3.IntegrityError:
             return False
 
-    def get_filter(self, filter_keys, kwargs):
-        if isinstance(filter_keys, str):
-            filter_keys = (filter_keys,)
-        return ' AND '.join("%s = %s" % (k, quoted(kwargs[k])) \
-                for k in filter_keys)
-
     # allow statements like:
     # db.update("topology", "mac", switch_mac=swmac, switch_port=swport)
-    def update(self, table, filter_keys, **kwargs):
-        tuples = self.get_tuples(table, kwargs)
-        filter_clause = self.get_filter(filter_keys, kwargs)
+    def update(self, table, primary_key_name, **kwargs):
+        cols, values = self.get_cols_and_values(table, kwargs)
+        values.append(kwargs[primary_key_name])
         cursor = self.c.cursor()
         cursor.execute("""
                 UPDATE %s
                 SET %s
-                WHERE %s;""" % (
+                WHERE %s = ?;""" % (
                     table,
-                    ','.join("%s = %s" % t for t in tuples),
-                    filter_clause))
+                    ','.join("%s = ?" % col for col in cols),
+                    primary_key_name),
+                    values)
         return cursor.rowcount
 
-    def get_where_clause(self, table, kwargs):
-        constraints = [ "%s=%s" % t for t in \
-                self.get_tuples(table, kwargs) ]
+    def get_where_clause_from_constraints(self, constraints):
         if len(constraints) > 0:
-            return "WHERE %s" % (
-                ' AND '.join(constraints));
+            return "WHERE %s" % (' AND '.join(constraints));
         else:
             return ""
+
+    # format a where clause with ANDs on the specified columns
+    def get_where_clause_pattern(self, cols):
+        constraints = [ "%s=?" % col for col in cols ]
+        return self.get_where_clause_from_constraints(constraints)
 
     # allow statements like:
     # mem_db.select("network", ip=ip)
     def select(self, table, **kwargs):
-        where_clause = self.get_where_clause(table, kwargs)
-        return self.c.execute("SELECT * FROM %s %s;" % (
-                    table, where_clause)).fetchall()
+        cols, values = self.get_cols_and_values(table, kwargs)
+        where_clause = self.get_where_clause_pattern(cols)
+        sql = "SELECT * FROM %s %s;" % (table, where_clause)
+        return self.c.execute(sql, values).fetchall()
 
     # same as above but expect only one matching record
     # and return it.
@@ -132,9 +117,10 @@ class SQLiteDB():
     # allow statements like:
     # db.delete("OpClass", name='Mean', daemon_id=1)
     def delete(self, table, **kwargs):
-        where_clause = self.get_where_clause(table, kwargs)
+        cols, values = self.get_cols_and_values(table, kwargs)
+        where_clause = self.get_where_clause_pattern(cols)
         sql = "DELETE FROM %s %s;" % (table, where_clause)
-        return self.c.execute(sql)
+        return self.c.execute(sql, values)
 
     def insert_or_update(self, table, filter_keys, **kwargs):
         num_updated = self.update(table, filter_keys, **kwargs)
