a76f1215a6a451acf846ba4f129b6ae886fdea69
Etienne Dublé <etienne.duble@imag.fr>
2017-11-16 13:02:11 +0100

    run-backend.sh: improve subprocess failure handling

--- test/run-backend.sh
+++ test/run-backend.sh
@@ -2,6 +2,7 @@
 
 cd $(dirname $0)/..
 TMPDIR=$(mktemp -d /tmp/tmp.XXXXXXXX)
+export PYTHONUNBUFFERED=1
 
 args="$@"
 
@@ -16,30 +17,60 @@ prefix_out()
     done
 }
 
-on_exit()
+on_ctrl_c()
 {
-    # in case of ctrl-c, the wait at the end of the script
-    # is cancelled, thus we get here, and the background
-    # processes receive SIGINT.
-
-    # we wait again to make sure the background processes
-    # have completed there cleanup procedure and exited.
-    wait
+    terminate
 }
 
-# whatever happens, call on_exit() at the end.
-trap on_exit EXIT
+trap on_ctrl_c SIGINT
+
+terminate_children()
+{
+    parent=$1
+    for child in $(pgrep --parent $parent)
+    do
+        terminate_children $child
+        kill $child 2>/dev/null && wait $child 2>/dev/null
+    done
+}
+
+terminate()
+{
+    terminate_children $$
+    kill $$
+}
+
+ensure_all_subprocesses_ok()
+{
+    ok=1
+    # check
+    for pid in $*
+    do
+        kill -0 $pid 2>/dev/null || ok=0
+    done
+    if [ $ok -eq 0 ]
+    then
+        terminate
+    fi
+}
 
 # run the commands in the background and prefix their
 # output.
 prefix_out HUB test/run-hub.sh $args &
+children=$!
 sleep 3
-prefix_out DAEMON0 test/run-daemon.sh 0 datasample spacetime & 
+ensure_all_subprocesses_ok $children
+prefix_out DAEMON0 test/run-daemon.sh 0 datasample spacetime &
+children="$children $!"
 sleep 0.2
+ensure_all_subprocesses_ok $children
 prefix_out DAEMON1 test/run-daemon.sh 1 mean map &
+children="$children $!"
 sleep 0.2
+ensure_all_subprocesses_ok $children
 prefix_out DAEMON2 test/run-daemon.sh 2 rscript &
+children="$children $!"
 
-# wait for background processes to complete
-wait
-
+# wait for 1 background process to complete
+wait -n
+terminate
