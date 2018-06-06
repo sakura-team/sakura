181be20e7821f435fda4a8a11202846b52412127
Etienne Dublé <etienne.duble@imag.fr>
2018-06-06 12:06:24 +0200

    Add "open" access scope.

--- sakura/common/access.py
+++ sakura/common/access.py
@@ -1,17 +1,17 @@
 import numpy as np
 from sakura.common.tools import make_enum
 
-ACCESS_SCOPES = make_enum('public', 'restricted', 'private')
+ACCESS_SCOPES = make_enum('open', 'public', 'restricted', 'private')
 GRANT_LEVELS = make_enum('hide', 'list', 'read', 'write', 'own')
 USER_TYPES = make_enum('anonymous', 'other', 'user_ro', 'user_rw', 'owner')
 
 ACCESS_TABLE = [
-# ACCESS_SCOPES:   public    | restricted | private    # USER_TYPES (column below)
-                  'list',     'list',      'hide',     # anonymous
-                  'read',     'list',      'hide',     # other
-                  'read',     'read',      'read',     # user_ro
-                  'write',    'write',     'write',    # user_rw
-                  'own',      'own',       'own'       # owner
+# ACCESS_SCOPES:   open      | public    | restricted | private    # USER_TYPES (column below)
+                  'list',     'list',     'list',      'hide',     # anonymous
+                  'write',    'read',     'list',      'hide',     # other
+                  'write',    'read',     'read',      'read',     # user_ro
+                  'write',    'write',    'write',     'write',    # user_rw
+                  'own',      'own',      'own',       'own'       # owner
 ]
 # re-format
 ACCESS_TABLE = np.array(list(map(
