7ef951fba9543582ea1609febb597a7d9c3027c0
Etienne Dublé <etienne.duble@imag.fr>
2018-07-25 14:26:06 +0200

    numpy stream: make it efficient with large arrays.

--- sakura/daemon/processing/streams/output/npstream.py
+++ sakura/daemon/processing/streams/output/npstream.py
@@ -18,18 +18,28 @@ def iter_uniq(names):
         yield name
 
 class NumpyArrayStream(OutputStreamBase):
-    def __init__(self, label, array):
+    def __init__(self, label, array, rows_cond = None):
         OutputStreamBase.__init__(self, label)
         self.array = array
         for col_label in array.dtype.names:
             col_type = array.dtype[col_label]
             self.add_column(col_label, col_type)
+        self.rows_cond = rows_cond
     def __iter__(self):
-        yield from self.array
+        for chunk in self.chunks():
+            yield from chunk
     def chunks(self, chunk_size = DEFAULT_CHUNK_SIZE, offset=0):
-        while offset < self.array.size:
-            yield self.array[offset:offset+chunk_size].view(NumpyChunk)
-            offset += chunk_size
+        if self.rows_cond is None:
+            # iterate over all rows
+            while offset < self.array.size:
+                yield self.array[offset:offset+chunk_size].view(NumpyChunk)
+                offset += chunk_size
+        else:
+            # iterate over selected rows
+            indices = self.rows_cond.nonzero()[0]
+            while offset < indices.size:
+                yield self.array[indices[offset:offset+chunk_size]].view(NumpyChunk)
+                offset += chunk_size
     def __select_columns__(self, *col_indexes):
         # caution: we may have strange requests here, such
         # as building a stream with twice the same column
@@ -53,6 +63,11 @@ class NumpyArrayStream(OutputStreamBase):
         # self.array[<col_label>] <comp_op> <other>
         # for example:
         # self.array['age'] > 20
-        array_cond = comp_op(self.array[col_label], other)
-        # then we apply this condition on the array
-        return NumpyArrayStream(self.label, self.array[array_cond])
+        # The result of this expression is a table of booleans
+        # indicating whether or not each row satisfies the condition.
+        rows_cond = comp_op(self.array[col_label], other)
+        # if we already add self.rows_cond, aggregate
+        if self.rows_cond is not None:
+            rows_cond = np.logical_and(rows_cond, self.rows_cond)
+        # return filtered object
+        return NumpyArrayStream(self.label, self.array, rows_cond)
