3fa8c720494b693d570438ddcc1aceb2d10b85c4
Etienne Dublé <etienne.duble@imag.fr>
2018-12-21 14:28:02 +0100

    Workaround mjpeg 1-frame-behind issue (chrome & firefox)

--- sakura/hub/web/video.py
+++ sakura/hub/web/video.py
@@ -9,13 +9,23 @@ def serve_video_stream(context, op_id):
             raise bottle.HTTPError(404, 'Invalid operator identifier.')
         print('serving video stream...')
         # let browser know our content type (i.e. motion jpeg format)
-        content_type = 'multipart/x-mixed-replace; boundary=frame'
+        content_type = 'multipart/x-mixed-replace; boundary=boundary'
         bottle.response.content_type = content_type
-        # yield frames from operator on daemon
-        for frame in op.stream_jpeg_frames():
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        # yield frames from operator on daemon.
+        # note: due to a bug in chrome and firefox
+        # (https://bugs.chromium.org/p/chromium/issues/detail?id=527446)
+        # a frame is not displayed before the header of the next one is
+        # received. That's why we first yield the multipart boundary and
+        # the content-type before waiting for the frame to be generated.
+        iterator = enumerate(op.stream_jpeg_frames())
+        while True:
+            yield (b'--boundary\r\n' +
+               b'Content-Type: image/jpeg\r\n\r\n')
+            i, frame = next(iterator)
+            print(i)
+            yield (frame + b'\r\n')
     except APIObjectDeniedError as e:
         raise bottle.HTTPError(403, str(e))
     except APIRequestError as e:
         raise bottle.HTTPError(400, str(e))
+    print('stream ended')
