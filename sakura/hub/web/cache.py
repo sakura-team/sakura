ad7fc1d9dccf6581e0469473b1aad40f8b649acc
Etienne Dublé <etienne.duble@imag.fr>
2019-01-17 15:53:03 +0100

    webcache: handle network issues on hub-side

--- sakura/hub/web/cache.py
+++ sakura/hub/web/cache.py
@@ -16,12 +16,15 @@ def webcache_serve(cdn, filepath):
         if not cachepath.parent.exists():
             cachepath.parent.mkdir(parents = True)
         url = WEBCACHE_CDNS[cdn] + '/' + filepath
+        print('webcache: fetching ' + url)
         try:
             with urllib.request.urlopen(url) as web_f:
                 with cachepath.open(mode='wb') as cache_f:
                     cache_f.write(web_f.read())
         except urllib.error.HTTPError as e:
             return bottle.abort(e.code, e.msg)
+        except urllib.error.URLError as e:
+            return bottle.abort(502, e.reason)
     # serve the file from the cache
     resp = bottle.static_file('/' + cachesubpath, root = webcache_dir)
     resp.headers['Access-Control-Allow-Origin'] = '*'
