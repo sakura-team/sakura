3fc537f9357da70204338b8b680b3e3269da0764
mike <michael.ortega@imag.fr>
2019-01-28 21:22:27 +0100

    SpaceTimeCube op: mouse passive in standalone op

--- operators/SpaceTimeCube/test_stc.py
+++ operators/SpaceTimeCube/test_stc.py
@@ -3,6 +3,7 @@
 import sys
 from sakura.common.gpu.openglapp import OpenglApp
 from stc.spacetimecube import SpaceTimeCube
+from sakura.common.gpu.openglapp import MouseMoveReporting
 
 if len(sys.argv) < 2:
     print("\33[1;31mERROR !! We need a csv file\33[m")
@@ -13,6 +14,9 @@ oapp.handler.debug = True
 
 oapp.init(800, 600)
 oapp.plan_periodic_task(oapp.handler.animation, .01)
+oapp.mouse_move_reporting = getattr(oapp.handler,
+                                    "mouse_move_reporting",
+                                    MouseMoveReporting.ALWAYS)
 oapp.handler.clean_data()
 oapp.handler.load_data(file=sys.argv[1])
 oapp.handler.update_floor()
