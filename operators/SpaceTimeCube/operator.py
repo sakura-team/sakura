f512f1ad8b739acbaeb29d430b524a8386654e84
mike <michael.ortega@imag.fr>
2019-04-10 16:16:05 +0200

    SpaceTimeCube: periodic task starts with wiggle activation

--- operators/SpaceTimeCube/operator.py
+++ operators/SpaceTimeCube/operator.py
@@ -11,6 +11,7 @@ class spacetimecubeOperator(Operator):
     SHORT_DESC = "Displays GPS trajectories, with time as a vertical component"
     TAGS = [ "visualisation"]
     def construct(self):
+        self.planned_task = False
         # inputs
         self.input_plug = self.register_input('GPS data')
         # parameters
@@ -28,7 +29,6 @@ class spacetimecubeOperator(Operator):
         self.register_tab('STC', 'spacetimecube.html')
         # opengl app
         self.ogl_app = OpenglApp(SpaceTimeCube())
-        self.ogl_app.plan_periodic_task(self.ogl_app.handler.animation, 1/60.)
         self.ogl_app.mouse_move_reporting = getattr(self.ogl_app.handler,
                                                     "mouse_move_reporting",
                                                     MouseMoveReporting.ALWAYS)
@@ -36,7 +36,6 @@ class spacetimecubeOperator(Operator):
         self.register_opengl_app(self.ogl_app)
 
     def init_op_data(self):
-
         # Cleaning data first
         self.ogl_app.handler.clean_data()
 
@@ -54,24 +53,34 @@ class spacetimecubeOperator(Operator):
         self.ogl_app.handler.update_floor()
 
     def handle_event(self, ev_type, **info):
-
         if ev_type == 'onload':
             self.init_op_data()
             return self.ogl_app.handler.get_trajectories()
+
         elif ev_type == 'wiggle':
             #TODO: implement a 'unplan' periodic task
             self.ogl_app.handler.toggle_wiggle(info['value'])
+            if not self.planned_task and info['value']:
+                self.planned_task = True
+                self.ogl_app.plan_periodic_task(self.ogl_app.handler.animation, 1/25.)
+
         elif ev_type == 'floor_darkness':
             self.ogl_app.handler.set_floor_darkness(info['value'])
+
         elif ev_type == 'cube_height':
             self.ogl_app.handler.set_cube_height(info['value'])
+
         elif ev_type == 'show_trajectories':
             self.ogl_app.handler.show_trajectories(info['value'])
+
         elif ev_type == 'hide_trajectories':
             self.ogl_app.handler.hide_trajectories(info['value'])
+
         elif ev_type == 'get_map_layers':
             return self.ogl_app.handler.get_map_layers()
+
         elif ev_type == 'set_map_layer':
             return self.ogl_app.handler.set_map_layer(info['value'])
+
         else:
             print('\33[1;31m!!!Unknown Event', ev_type, '!!!\33[m')
