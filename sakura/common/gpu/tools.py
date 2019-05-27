c419d8c52ec0a840578a579141b499643ff00dd9
Etienne Dublé <etienne.duble@imag.fr>
2019-05-27 16:54:55 +0200

    gpu.tools: add write_bmp()

--- sakura/common/gpu/tools.py
+++ sakura/common/gpu/tools.py
@@ -1,6 +1,7 @@
 import numpy as np
 import OpenGL.GL as gl
 from PIL import Image
+import struct
 
 PPM_HEADER = """\
 P6
@@ -34,3 +35,20 @@ class JPEGWriter:
         im.save(f, 'JPEG', **params)
 
 write_jpg = JPEGWriter()
+
+class BMPWriter(struct.Struct):
+    def __init__(self):
+        self.headers_struct = struct.Struct("<2sIHHIIIIHHIIIIII")
+    def __call__(self, f, width, height):
+        data_offset = 14 + 40
+        row_padding_len = (4 - (width * 3)) % 4
+        length = data_offset + height * (width * 3 + row_padding_len)
+        headers = self.headers_struct.pack(
+                b'BM', length, 0, 0, data_offset,
+                40, width, height, 1, 24, 0, 0, 10000, 10000, 0, 0)
+        f.write(headers)
+        args = (0, 0, width, height, gl.GL_BGR, gl.GL_UNSIGNED_BYTE)
+        array = gl.glReadPixels(*args)
+        f.write(array)
+
+write_bmp = BMPWriter()
