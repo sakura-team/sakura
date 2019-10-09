9f9308b2d3fa5613f9e94fb0664200f8934789e1
mike <michael.ortega@imag.fr>
2019-10-09 09:50:04 +0200

    wip on sending trajectories to map

--- operators/gps2d/js/gps2d.js
+++ operators/gps2d/js/gps2d.js
@@ -1,8 +1,23 @@
+var trajectories = []
+var gps2d_map = null;
+
+function update_trajectories(data) {
+    console.log('toto');
+    gps2d_map.setView([51.505, -0.09], 13);
+}
+
 function init_gps2d() {
-    var mymap = L.map('gps2d_map_div');
+
+    //Get data
+    sakura.apis.operator.fire_event('get_data_first').then(
+        update_trajectories
+    );
+
+    //map creation
+    gps2d_map = L.map('gps2d_map_div');
     L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
         maxZoom: 18
-        }).addTo(mymap);
-    mymap.setView([51.505, -0.09], 13);
+        }).addTo(gps2d_map);
+    gps2d_map.setView([51.505, -0.09], 13);
 }
