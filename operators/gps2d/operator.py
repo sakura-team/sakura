31a5c8faa6fe0624dbd000839a5c76061e5b9e43
mike <michael.ortega@imag.fr>
2019-10-09 09:50:04 +0200

    wip on sending trajectories to map

--- operators/gps2d/operator.py
+++ operators/gps2d/operator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from sakura.daemon.processing.operator import Operator
 from sakura.daemon.processing.parameter import NumericColumnSelection
+from sakura.daemon.processing.parameter import StrColumnSelection
 from sakura.daemon.processing.source import ComputedSource
 from numpy.lib import recfunctions
 
@@ -18,7 +19,7 @@ class gps2d(Operator):
 
         # parameters
         self.input_column_param_id = self.register_parameter(
-                NumericColumnSelection('Trajectory ids', self.input))
+                StrColumnSelection('Trajectory ids', self.input))
         self.input_column_param_lon = self.register_parameter(
                 NumericColumnSelection('longitude', self.input))
         self.input_column_param_lon = self.register_parameter(
@@ -29,6 +30,29 @@ class gps2d(Operator):
 
         self.iterator = None
 
-    #def handle_event(self, ev_type):
-    #    print(ev_type, time_credit)
-    #    return {}
+    def handle_event(self, ev_type):
+        print(ev_type)
+        if not self.input.connected():
+            return { 'issue': 'NO DATA: Input is not connected.' }
+
+        if 'get_data' in ev_type:
+            if ev_type == 'get_data_first':
+                #Init iterator
+                column_id       = self.input_column_param_id.col_index
+                column_x        = self.input_column_param_lon.col_index
+                column_y        = self.input_column_param_lat.col_index
+                source          = self.input.source
+                source          = source.select_columns(column_id, column_x, column_y)
+                self.iterator   = source.chunks()
+
+            chunk = None
+            try:
+                print('here')
+                chunk = self.iterator.next()
+                print('there')
+
+                return {'db': chunk, 'end': False}
+            except:
+                print('issue')
+                return {'db': null, 'end': True}
+        return {}
