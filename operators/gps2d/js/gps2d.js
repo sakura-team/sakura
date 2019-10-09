6088998a3390adaf1929f86171ff95d9a1b97f90
mike <michael.ortega@imag.fr>
2019-10-09 17:11:59 +0200

    gps2d: first polylines

--- operators/gps2d/js/gps2d.js
+++ operators/gps2d/js/gps2d.js
@@ -6,17 +6,22 @@ var center = null;
 
 function update_trajectories(chunk) {
 
-  if (center === null)
-      center = chunk.mean;
-  else if (chunk.mean){
-      center = [(center[0] + chunk.mean[0]) /2,
-                (center[1] + chunk.mean[1]) /2]
-  }
-  gps2d_map.panTo(center);
+  if (chunk.db !== null) {
+      if (center === null)
+          center = chunk.mean;
+      else if (chunk.mean){
+          center = [(center[0] + chunk.mean[0]) /2,
+                    (center[1] + chunk.mean[1]) /2]
+      }
+      var coords = chunk.db.map(t => ([t[1], t[2]]));
+      var polygon = L.polyline(coords, {color: 'red',
+                                        weight: 3,
+                                        opacity: 0.5,
+                                        smoothFactor: 1}).addTo(gps2d_map);
+      gps2d_map.panTo(center);
 
-  if (chunk.end == false) {
       sakura.apis.operator.fire_event('get_data_continue').then(
-          update_trajectories
+        update_trajectories
       );
   }
   else {
