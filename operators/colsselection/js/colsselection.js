260a598dccb04d422dda2c3616c810a31f70c97a
mike <michael.ortega@imag.fr>
2019-10-22 15:53:28 +0200

    cols selection: displaying input table + firing selected columns back to py

--- operators/colsselection/js/colsselection.js
+++ operators/colsselection/js/colsselection.js
@@ -1,59 +1,83 @@
-var main_data = { 'col1': ['A', 'B', 'C'],
-                  'col2': ['D', 'E', 'F'],
-                  'col3': ['G', 'H', 'I']}
+var main_data = { 'headers': ['col1', 'col2', 'col3', 'col4'],
+                  'rows': [ ['A','B','C',0],
+                            ['D','E','F',1],
+                            ['G','H','I',2],
+                            ['J','K','L',3]]}
+
+var selected_columns = [];
 
 function init() {
+  sakura.apis.operator.fire_event('get_data', 0.0).then( fill_table );
+}
+
+function fill_table(result) {
+
+    main_data = result;
     var thead = $('#main_table_head');
     var tbody = $('#main_table_body');
     thead.empty();
     tbody.empty();
 
     //HEAD
-    Object.keys(main_data).forEach( function(key, index){
+    main_data.headers.forEach( function(key, index){
         var th = $('<th>', {'html': '<font color="black">'+key+'</font>',
-                            'onclick': 'select_col(\''+key+'\','+index+')',
-                            'style': 'cursor: pointer;',
-                            'bgcolor': 'white'});
+                            'onclick': 'select_col('+index+')',
+                            'style': 'cursor: pointer; text-align: center;',
+                            'bgcolor': 'lightgrey'});
         th.data('selected', false);
         thead.append(th);
     });
 
     //BODY
-    Object.keys(main_data).forEach( function(key){
+    main_data.rows.forEach( function(row){
         var tr = $('<tr>');
-        main_data[key].forEach( function (elt) {
-            var td = $('<td>', {'html': '<font color="black">'+elt+'</font>'});
+        row.forEach( function (elt, index) {
+            var td = $('<td>', {'html': '<font color="black">'+elt+'</font>',
+                                'onclick': 'select_col('+index+')',
+                                'style': 'cursor: pointer; text-align: center; padding: 0px;'});
             tr.append(td);
         });
         tbody.append(tr);
     });
 }
 
-function select_col(key, index) {
+function select_col(col) {
     var tbody = $('#main_table_body');
-    var th = $($('#main_table_head').children()[index]);
+    var th = $($('#main_table_head').children()[col]);
     th.data('selected', !th.data('selected'));
+
+    console.log(col);
+    if (selected_columns.indexOf(col) == -1) {
+        selected_columns.push(col);
+    }
+    else {
+        var i = selected_columns.indexOf(col);
+        selected_columns.splice (i, 1);
+    }
+    console.log(selected_columns);
 
     if (th.data('selected')) {
         th.attr('bgcolor', '#4285F4');
         th[0].innerHTML = th[0].innerHTML.replace('black', 'white');
 
         for (var i=0; i<tbody[0].children.length; i++) {
-            var cell = $(tbody[0].children[i].cells[index]);
+            var cell = $(tbody[0].children[i].cells[col]);
             cell.attr('bgcolor', '#4285F4');
             cell[0].innerHTML = cell[0].innerHTML.replace('black', 'white');
         }
     }
     else {
-        th.attr('bgcolor', 'white');
+        th.attr('bgcolor', 'lightgrey');
         th[0].innerHTML = th[0].innerHTML.replace('white', 'black');
 
         for (var i=0; i<tbody[0].children.length; i++) {
-            var cell = $(tbody[0].children[i].cells[index]);
+            var cell = $(tbody[0].children[i].cells[col]);
             cell.attr('bgcolor', 'white');
             cell[0].innerHTML = cell[0].innerHTML.replace('white', 'black');
         }
     }
-
-
+    console.log('HHHHHHHH');
+    sakura.apis.operator.fire_event('set_cols', 0.0, {'cols': selected_columns}).then( function(result) {
+        console.log('titi');
+    });
 }
--- operators/colsselection/operator.py
+++ operators/colsselection/operator.py
@@ -9,14 +9,36 @@ import numpy as np
 class colsselection(Operator):
     NAME = "Columns Selection"
     SHORT_DESC = "Select columns from a table."
-    TAGS = ["filter"]
+    TAGS = ["Filter"]
 
     def construct(self):
-        # inputs
-        self.input = self.register_input('Input table')
+
+        self.selected_columns = []
+
+        # input
+        self.input_plug = self.register_input('Input table')
+
+        # output
+        self.output_plug = self.register_output('Output table')
+        #output_source = ComputedSource('Output table', self.compute)
 
         # additional tabs
-        self.register_tab('Select', 'colsselection.html')
+        self.register_tab('Selection', 'colsselection.html')
 
-    def handle_event(self, ev_type):
-        return {}
+    def handle_event(self, ev_type, time_credit, **info):
+        if ev_type == 'get_data':
+            source = self.input_plug.source
+            headers = []
+            for c in self.input_plug.columns:
+                headers.append(c._label)
+
+            it = next(source.chunks())
+            return {'headers': headers, 'rows': it}
+
+        elif ev_type == 'set_cols':
+            self.selected_columns = info['cols']
+        else:
+            print('unknown event', ev_type)
+
+    def compute(self):
+        pass
