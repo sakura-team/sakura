cb38c713df96c57f28dc4f53b260d2a22f8054f7
mike <michael.ortega@imag.fr>
2020-03-20 08:13:50 +0100

    operator colsselection: dealing with no selection

--- operators/colsselection/operator.py
+++ operators/colsselection/operator.py
@@ -6,12 +6,15 @@ from numpy.lib import recfunctions
 from time import time
 import numpy as np
 
+DEBUG = False
+
 class colsselection(Operator):
     NAME = "Columns Selection"
     SHORT_DESC = "Select columns from a table."
     TAGS = ["Filter"]
 
     def construct(self):
+        if DEBUG: print('colsselection -- CONSTRUCT')
         self.selected_columns = []
         # input
         self.input_plug = self.register_input('Input table')
@@ -21,6 +24,8 @@ class colsselection(Operator):
         self.register_tab('Selection', 'colsselection.html')
 
     def handle_event(self, ev_type, time_credit, **info):
+        if DEBUG: print('colsselection -- EVENT', ev_type)
+
         if not self.input_plug.connected():
             return { 'issue': 'NO DATA: Input is not connected.' }
 
@@ -29,7 +34,7 @@ class colsselection(Operator):
             headers = []
             for c in self.input_plug.columns:
                 headers.append(c._label)
-
+            if DEBUG: print('colsselection -- HEADERS', headers)
             it = next(source.chunks())
             return {'headers': headers,
                     'rows': it[:20],
@@ -41,6 +46,8 @@ class colsselection(Operator):
             if len(self.selected_columns):
                 source = self.input_plug.source
                 self.output_plug.source = source.select_columns(*self.selected_columns)
+            else:
+                self.output_plug.source = None
             return {}
         else:
             print('unknown event', ev_type)
