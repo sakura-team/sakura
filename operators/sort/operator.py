9799fc0db256de212bb2e1ff7f4e0ee0f47f4635
Etienne Dublé <etienne.duble@imag.fr>
2020-06-29 10:41:29 +0200

    Sort op: update management of events.

--- operators/sort/operator.py
+++ operators/sort/operator.py
@@ -15,9 +15,12 @@ class SortOperator(Operator):
         self.column_param.on_change.subscribe(self.update_output)
 
     def update_output(self):
-        source = self.input_plug.source
         input_column = self.column_param.column
-        # apply sort
-        source = source.sort(input_column)
-        # update output source
-        self.output_plug.source = source
+        if input_column is None:
+            self.output_plug.source = None
+        else:
+            source = self.input_plug.source
+            # apply sort
+            source = source.sort(input_column)
+            # update output source
+            self.output_plug.source = source
